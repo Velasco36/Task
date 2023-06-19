7508a0092f674c287d23ed0e3efd767a0ca82cdd
Miguel Velasco <migue16velasco@gmail.com>
2023-06-19 07:49:28 -0400

    fixed: refactorizar search.jsx

--- src/components/Task/New Task/Search.jsx
+++ src/components/Task/New Task/Search.jsx
@@ -1,40 +1,13 @@
-import React from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 export function Search() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <button
-        type='submit'
-        style={{
-          marginTop: '50px',
-          width: '20%',
-          height: '50px',
-          borderRadius: '40px',
-          cursor: 'pointer',
-          border: '0px',
-          background: 'linear-gradient(2deg, rgba(0, 38, 240, 2), rgba(0, 0, 0, 3))',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <SearchIcon style={{ marginRight: '5px' }} />
-        <input
-          type="text"
-          placeholder="Buscar"
-          style={{
-            border: 'none',
-            outline: 'none',
-            background: 'transparent',
-            color: '#fff',
-            width: '80%',
-          }}
-        />
+    <div className="center">
+      <button type="submit" className="button-search">
+        <SearchIcon style={{ marginRight: "5px" }} />
+        <input type="text" placeholder="Buscar" className="input-search" />
       </button>
     </div>
   );
 }
-
-   
-
