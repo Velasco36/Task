7c185653dd41a88c9d7367307959fd45fa9157a0
Miguel Velasco <migue16velasco@gmail.com>
2023-06-20 12:40:05 -0400

    fixed: change body by bg

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Headers } from "./Headers/Headers";
 import { ButtonTask } from "./New Task/ButtonTask";
-
-
 import "./style.css";
 import ListCard from "./Cards/Background";
 
 export function Task() {
   return (
-    <div>
+    <div className="bg">
       <Headers />
       <h1 className="list" >
         Task List
