9c9ee3a821a39f004a225fb0be4ce90bc8092249
Miguel Velasco <migue16velasco@gmail.com>
2023-08-07 03:47:15 -0400

    fix: change styles headers

--- src/components/Task/New Task/ButtonTask.jsx
+++ src/components/Task/New Task/ButtonTask.jsx
@@ -1,15 +1,17 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 export function ButtonTask() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <button type="submit" className="button">
-        <Link to="/home">
-          <h2>New Task</h2>
-        </Link>
-      </button>
+    <div className="button-container">
+      <div className="button-wrapper">
+        <button type="submit" className="button">
+          <Link to="/home">
+            <h2 className="title">New Task</h2>
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
+
--- src/components/Task/New Task/Search.jsx
+++ src/components/Task/New Task/Search.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import './style.css'
 
 export function Search() {
   return (
     <div className="center">
       <button type="submit" className="button-search">
-        <SearchIcon style={{ marginRight: "5px" }} />
+        <SearchIcon className="icons-search" />
         <input type="text" placeholder="Buscar" className="input-search" />
       </button>
     </div>
