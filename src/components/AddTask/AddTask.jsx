4b07b109ad896321b050de077b5eba60afae2303
Miguel Velasco <migue16velasco@gmail.com>
2023-08-07 03:43:20 -0400

    fix:add  headers  AddTask

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-// import { Headers } from '../Task/Headers/Headers'
 import { CardDesing } from './CardDesign/CardDesign'
 import "./style.css"
+import {Headers} from "./../Task/Headers/Headers"
 
 
 export default function AddTask() {
   return (
     <>
-    <div className='background'>
-
-    <CardDesing />
+      <div className="app-container background">
+     <Headers/>
+      <div className='background'>
+        <CardDesing />
+      </div>
     </div>
-
     </>
 
-  )
+  );
 }
