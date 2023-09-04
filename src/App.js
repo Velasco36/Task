e349f499cbeea2a1bd6b209f0d29c574016d6a1a
Miguel <migue16velasco@gmail.com>
2023-09-03 22:25:46 -0400

    fix: add routers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 import React from 'react';
 import AddTask from './components/AddTask/AddTask';
 import { Task } from './components/Task';
+import { Login } from './components/Login/index.jsx';
+import {Register} from './components/Register/index'
 
 
 function App() {
   return (
     <>
-    
+
     <Routes>
     <Route path='/' element={<Task />} />
     <Route path='/home' element={<AddTask />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
     </Routes>
 
     </>
