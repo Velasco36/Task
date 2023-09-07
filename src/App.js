b947d950791a22a83d19aa8b87a7b86a873e2a36
Gian vespa <givespas@gmail.com>
2023-09-07 18:04:49 -0500

    fix<app>: prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 // App.js
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import React from 'react';
-import AddTask from './components/AddTask/AddTask';
-import { Task } from './components/Task';
-import { Login } from './components/Login/index.jsx';
-import {Register} from './components/Register/index'
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import AddTask from "./components/AddTask/AddTask";
+import { Task } from "./components/Task";
+import { Login } from "./components/Login/index.jsx";
+import { Register } from "./components/Register/index";
 
 function App() {
   return (
-    <>
-
     <Routes>
-    <Route path='/' element={<Task />} />
-    <Route path='/home' element={<AddTask />} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/register' element={<Register />} />
+      <Route path="/" element={<Task />} />
+      <Route path="/home" element={<AddTask />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
-
-    </>
   );
 }
 
