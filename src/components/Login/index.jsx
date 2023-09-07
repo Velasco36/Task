0669bd2ea299df28820c9ff6f91985c6f0abd758
Miguel <migue16velasco@gmail.com>
2023-09-07 18:29:44 -0400

    fix: conf axios login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./index.css";
-import axios from "axios";
-
+import clientAxios from "../../config/axios";
 
 export function Login() {
   const [nick_name, setnick_name] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // Nuevo estado para mostrar mensajes de error
+
 
   const handlenick_nameChange = (e) => {
     setnick_name(e.target.value);
@@ -14,41 +15,68 @@ export function Login() {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
-
-
   const handleSubmitButton = async () => {
     if (nick_name === "" || password === "") {
-      // Tu lógica de validación aquí
-      console.log("Por favor complete todos los campos requeridos.");
+      setError("Por favor complete todos los campos requeridos.");
       return;
     }
 
-    console.log("nick_name:", nick_name);
-    console.log("Contraseña:", password);
+
+
+    const data = JSON.stringify({
+      "nick_name": nick_name,
+      "password": password
+    });
 
     try {
+      const response = await clientAxios.post('login', data);
+      console.log(response)
 
-     const user = {
-      nick_name: nick_name,
-      password: password,
-      }
-      console.log(user)
-      const response = await axios.post('http://localhost:3333/login', user);
-
-      console.log(response);
-      // const token = response.data.token;
-      // // Guardar el token en localStorage
-      // window.localStorage.setItem("token", token);
-
-      // Redirigir después del inicio de sesión exitoso
-      console.log("Inicio de sesión exitoso. Redireccionando...");
-      // Puedes usar react-router-dom para redirigir a una página diferente si es necesario.
-    } catch (error) {
-      console.log("Error en la solicitud de inicio de sesión:", error);
-      // Manejar errores de inicio de sesión aquí, por ejemplo, mostrar un mensaje de error al usuario.
-      console.log(error)
+    }catch(e){
+      console.log(e)
+      
     }
-  };
+
+
+
+
+
+  }
+
+
+  // const handleSubmitButton = async () => {
+  //   if (nick_name === "" || password === "") {
+  //     // Tu lógica de validación aquí
+  //     console.log("Por favor complete todos los campos requeridos.");
+  //     return;
+  //   }
+
+  //   console.log("nick_name:", nick_name);
+  //   console.log("Contraseña:", password);
+
+  //   try {
+
+  //    const user = {
+  //     nick_name: nick_name,
+  //     password: password,
+  //     }
+  //     console.log(user)
+  //     const response = await axios.post('http://localhost:3333/login', user);
+
+  //     console.log(response);
+  //     // const token = response.data.token;
+  //     // // Guardar el token en localStorage
+  //     // window.localStorage.setItem("token", token);
+
+  //     // Redirigir después del inicio de sesión exitoso
+  //     console.log("Inicio de sesión exitoso. Redireccionando...");
+  //     // Puedes usar react-router-dom para redirigir a una página diferente si es necesario.
+  //   } catch (error) {
+  //     console.log("Error en la solicitud de inicio de sesión:", error);
+  //     // Manejar errores de inicio de sesión aquí, por ejemplo, mostrar un mensaje de error al usuario.
+  //     console.log(error)
+  //   }
+  // };
 
   return (
     <div id="background">
--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 
 const clientAxios = axios.create({
-    baseURL:`http://127.0.0.1:3333/`
+    baseURL:'http://localhost:3333/',
+    timeout: 5000,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
 })
 export default clientAxios
+
+
