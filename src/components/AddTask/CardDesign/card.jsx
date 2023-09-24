38cce8ca64292a6de840219c32a13609c25c3cb0
Miguel Velasco <migue16velasco@gmail.com>
2023-09-24 07:43:14 -0400

    feat: add spinner

--- src/components/AddTask/CardDesign/card.jsx
+++ src/components/AddTask/CardDesign/card.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import {useSelector, useDispatch} from "react-redux"
-import { updateDescription, updateTitle } from '../../../redux/actions'
-import './style.css'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateDescription, updateTitle } from "../../../redux/actions";
+import "./style.css";
 
-export function Card({ showButton, handleSubmit }) {
+export function Card({ showButton, handleSubmit, load}) {
 
-  
 
-  const title = useSelector((state) => state.title)
-  const description = useSelector((state) => state.description)
+  const title = useSelector((state) => state.title);
+  const description = useSelector((state) => state.description);
 
   const dispatch = useDispatch();
 
-
   return (
-    <form className="form" id='fom'>
+    <form className="form" id="form">
       <div className="input-container">
         <input
           className="title border"
           type="text"
           id="color-title"
           value={title}
-          placeholder=''
-          onChange={(e) => dispatch(updateTitle((e.target.value)))}
+          placeholder=""
+          required
+          onChange={(e) => dispatch(updateTitle(e.target.value))}
         />
         <span className="floating-label">Title</span>
       </div>
@@ -31,27 +30,31 @@ export function Card({ showButton, handleSubmit }) {
         <textarea
           className="description border"
           value={description}
-          placeholder=''
-          onChange={(e) => dispatch(updateDescription((e.target.value)))}
+          placeholder=""
+          required
+          onChange={(e) => dispatch(updateDescription(e.target.value))}
+          rows={5}
+          cols={50}
         />
         <span className="floating-label">Description</span>
       </div>
 
+      {load && (
+        <div className="containerloader">
+          <span className="loader"></span>
+        </div>
+      )}
+
       {showButton && (
         <button
           id="button"
-          className='button'
+          className="button"
           type="submit"
           onClick={handleSubmit}
-
         >
           Submit
         </button>
       )}
     </form>
-  )
-
+  );
 }
-
-
-
