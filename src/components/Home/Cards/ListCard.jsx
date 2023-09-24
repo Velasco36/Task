a1d9d79da3da4e8d1ce04c5fbcdd0914d3cfe2d6
Miguel Velasco <migue16velasco@gmail.com>
2023-09-24 17:32:30 -0400

    feat:<cards/listCard> apply the short method

--- src/components/Home/Cards/ListCard.jsx
+++ src/components/Home/Cards/ListCard.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
 import clientAxios from "../../../config/axios";
 import { Card } from "./Card";
 import "./style.css";
 
 export default function ListCard() {
-  const [Tasks, setTask_back] = useState([]);
-  // const Users = useSelector((state) => state.users);
-  // console.log(Users)
+  const [tasks, setTasks] = useState([]);
   const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -17,10 +14,13 @@ export default function ListCard() {
           const response = await clientAxios.get("tasks", {
             headers: { Authorization: `Bearer ${token}` },
           });
-          const tasksData  = response.data.data.tasks
+          const tasksData = response.data.data.tasks;
 
-          setTask_back(tasksData)
+          tasksData.sort((a, b) => {
+            return a.state === "anchored" && b.state !== "anchored" ? -1 : 1;
+          });
 
+          setTasks(tasksData);
         } else {
           console.log("No se encontró un token en el localStorage.");
         }
@@ -33,11 +33,10 @@ export default function ListCard() {
 
   return (
     <div className="grid-container">
-      {Tasks.length === 0 && <div className="empty-placeholder"></div>}
-      {Tasks.map((Task, index) => (
-        <Card key={index} id={Task.id} title={Task.name} body={Task.description} color={Task.color} />
+      {tasks.length === 0 && <div className="empty-placeholder"></div>}
+      {tasks.map((task, index) => (
+        <Card key={index} id={task.id} title={task.name} body={task.description} color={task.color} state={task.state}/>
       ))}
-
     </div>
   );
 }
