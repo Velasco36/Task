dbf0092290d27e21fc2ca10bc558f2c07e11fd44
Miguel Velasco <migue16velasco@gmail.com>
2023-09-26 08:30:33 -0400

    feat:<card/card> add edit title and description

--- src/components/Home/Cards/Card.jsx
+++ src/components/Home/Cards/Card.jsx
@@ -1,70 +1,74 @@
 import React from "react";
 import { useState } from "react";
 import dayjs from "dayjs";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import AnchorIcon from "@mui/icons-material/Anchor";
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-import 'sweetalert2/src/sweetalert2.scss'
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import "./style.css";
-import { useDispatch } from "react-redux";
-import { Id_task } from "../../../redux/actions";
+// import { useDispatch } from "react-redux";
+// import { updateDescription, updateTitle } from "../../../redux/actions";
+// import { Id_task } from "../../../redux/actions";
 import clientAxios from "../../../config/axios";
 
-export const Card = ({ title, body, color, id, state, date  }) => {
-  const dispatch = useDispatch()
+export const Card = ({ title, body, color, id, state, date }) => {
+  const [cardTitle, setCardTitle] = useState(title);
+  const [cardDescriptions, setDescriptions] = useState(body);
+  // const dispatch = useDispatch();
 
   const token = localStorage.getItem("token");
-  const fechaFormateada = dayjs(date).format('YYYY-MM HH:mm');
-  console.log(fechaFormateada)
-  const [anchor, setanchor] = useState(state === 'pending' ? 'anchored' : 'pending');
-  const hableEdit = async () => {
-    dispatch(Id_task(id))
-  }
-  const handleAnchorClick = async () => {
-    setanchor((prevAnchor) => {
-      return prevAnchor === "pending" ? "anchored" : "pending";
-    });
+  const fechaFormateada = dayjs(date).format("YYYY-MM HH:mm");
+  const [anchor, setAnchor] = useState(
+    state === "pending" ? "anchored" : "pending"
+  );
+
+  const handleClick = async () => {
+    const newAnchor = anchor === "pending" ? "anchored" : "pending";
+    setAnchor(newAnchor);
 
     const data = {
-      state: anchor
-    }
+      name: cardTitle,
+      description: cardDescriptions,
+      state: anchor,
+    };
 
     try {
-      const response = await clientAxios.put(`tasks/${id}`, data, { headers: { Authorization: `Bearer ${token}` }, });
-      console.log(response)
+      const response = await clientAxios.put(`tasks/${id}`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(response);
       Swal.fire({
-        title: 'Anchored!',
-        text: 'Se ha fijado correctamente',
-        icon: 'success',
-        // confirmButtonText: 'success'
-      })
-      setTimeout(() => {
-        window.location.replace("/");
-      }, 1000);
-
-      console.log('Eliminado correctamente');
+        title: "Success!",
+        text: "Se ha editado correctamente",
+        icon: "success",
+        confirmButtonText: "Confirmar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.replace("/");
+        }
+      });
     } catch (e) {
       console.error("Error al obtener los datos:", e);
     }
   };
-
   const handleDeleteClick = async () => {
     try {
-      const response = await clientAxios.delete(`tasks/${id}`, { headers: { Authorization: `Bearer ${token}`},});
-      console.log(response)
-
+      const response = await clientAxios.delete(`tasks/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
       Swal.fire({
-        title: 'Alert!',
-        text: 'se ha eliminado correctamente',
-        icon: 'success',
-        // confirmButtonText: 'success'
-      })
+        title: "Alert!",
+        text: "se ha eliminado correctamente",
+        icon: "success",
+        confirmButtonText: "success",
+      });
+      console.log(response);
       setTimeout(() => {
         window.location.replace("/");
       }, 1000);
 
-      console.log('elimidano correctamente');
+      console.log("elimidano correctamente");
     } catch (e) {
       console.error("Error al obtener los datos:", e);
     }
@@ -73,13 +77,20 @@ export const Card = ({ title, body, color, id, state, date  }) => {
     <div className="card-container" style={{ backgroundColor: color }}>
       <div className="icon-container" id="icons">
         <div className="letf">
-          <DeleteOutlineIcon style={{color: 'white'}} onClick={handleDeleteClick} />
+          <DeleteOutlineIcon
+            style={{ color: "white" }}
+            onClick={handleDeleteClick}
+          />
         </div>
         <p className="text">Add Task</p>
         <br />
         <p className="date">Date: {fechaFormateada}</p>
-        <div className="right " >
-          <AnchorIcon  onClick={handleAnchorClick} style={{ color : state==='anchored' ? 'red' : 'white' }}  />
+        <div className="right ">
+          <AnchorIcon
+
+            onClick={handleClick}
+            style={{ color: state === "anchored" ? "red" : "white" }}
+          />
         </div>
       </div>
       <div className="input-container">
@@ -89,8 +100,8 @@ export const Card = ({ title, body, color, id, state, date  }) => {
             id="title"
             type="text"
             placeholder=" "
-            value={title}
-            onChange={() => {}}
+            value={cardTitle}
+            onChange={(e) => setCardTitle(e.target.value)}
           />
           <span className="floating-label">Title</span>
         </div>
@@ -98,17 +109,20 @@ export const Card = ({ title, body, color, id, state, date  }) => {
           <textarea
             className="description border"
             placeholder=" "
-            value={body}
-            onChange={() => {}}
+            value={cardDescriptions}
+            onChange={(e) => setDescriptions(e.target.value)}
           />
           <span className="floating-label">Description</span>
         </div>
 
         <div className="input-container">
-          <Link to="/home"><button className="add-card-btn" onClick={hableEdit}>edit</button></Link>
+          <Link to="/">
+            <button className="add-card-btn" onClick={handleClick}>
+              edit
+            </button>
+          </Link>
         </div>
       </div>
-
     </div>
   );
 };
