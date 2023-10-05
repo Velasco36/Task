c28e489d77a70a72f22d3876e8d98aec56da6f20
Miguel Velasco <migue16velasco@gmail.com>
2023-10-05 17:47:21 -0400

    feat:<api/deleteTask> add api delete

---
+++ src/components/api/deleteTask.js
@@ -0,0 +1,22 @@
+import clientAxios from '../../config/axios';
+import "sweetalert2/src/sweetalert2.scss";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+
+export const deleteTask = async (id) => {
+    const token = localStorage.getItem('token');
+    const response = await clientAxios.delete(`tasks/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      Swal.fire({
+        title: "Alert!",
+        text: "se ha eliminado correctamente",
+        icon: "success",
+        confirmButtonText: "success",
+      });
+      console.log(response);
+      setTimeout(() => {
+        window.location.replace("/");
+      }, 1000);
+  return response.data
+}
+
