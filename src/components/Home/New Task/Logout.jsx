07b4f37b634bb6f7e51f81d7f5d9b354d0819456
Miguel Velasco <migue16velasco@gmail.com>
2023-10-06 19:59:41 -0400

    feat:<home/newtask>add tool

--- src/components/Home/New Task/Logout.jsx
+++ src/components/Home/New Task/Logout.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import './style.css'
-import LogoutIcon from '@mui/icons-material/Logout';
-
+import "./style.css";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Tool } from "./tool";
 export function Logout() {
-
-    const handleSubmit = () => {
-        console.log('click')
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        setTimeout(() => {
-            window.location.replace("/login");
-        }, 1000);
-    }
+  const handleSubmit = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setTimeout(() => {
+      window.location.replace("/login");
+    }, 1000);
+  };
   return (
     <div className="logout">
-
-        <LogoutIcon onClick={handleSubmit} className="large-icon"/>
+      <LogoutIcon onClick={handleSubmit} className="large-icon" />
+      <Tool className="large-icon" />
     </div>
   );
 }
