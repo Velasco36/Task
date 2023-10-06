c155c27d78ed6ffdbd67db7efeae1e6677d3a63c
Miguel Velasco <migue16velasco@gmail.com>
2023-10-06 10:21:19 -0400

    feat:<card>/cards.jsx

--- src/components/Home/Cards/Card.jsx
+++ src/components/Home/Cards/Card.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { useState } from "react";
 import dayjs from "dayjs";
-import { Link } from "react-router-dom";
+import { CardForm } from "./cardForm";
 import AnchorIcon from "@mui/icons-material/Anchor";
-import Swal from "sweetalert2/dist/sweetalert2.js";
-import "sweetalert2/src/sweetalert2.scss";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import "./style.css";
-// import { useDispatch } from "react-redux";
-// import { updateDescription, updateTitle } from "../../../redux/actions";
-// import { Id_task } from "../../../redux/actions";
-import clientAxios from "../../../config/axios";
+import { putTask } from "../../api/putTask";
+import { deleteTask } from "../../api/deleteTask";
 
 export const Card = ({ title, body, color, id, state, date }) => {
   const [cardTitle, setCardTitle] = useState(title);
   const [cardDescriptions, setDescriptions] = useState(body);
-  // const dispatch = useDispatch();
-
-  const token = localStorage.getItem("token");
   const fechaFormateada = dayjs(date).format("YYYY-MM HH:mm");
+  
   const [anchor, setAnchor] = useState(
     state === "pending" ? "anchored" : "pending"
   );
@@ -34,40 +28,15 @@ export const Card = ({ title, body, color, id, state, date }) => {
     };
 
     try {
-      const response = await clientAxios.put(`tasks/${id}`, data, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      console.log(response);
-      Swal.fire({
-        title: "Success!",
-        text: "Se ha editado correctamente",
-        icon: "success",
-        confirmButtonText: "Confirmar",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.replace("/");
-        }
-      });
+        await putTask(id, data)
     } catch (e) {
       console.error("Error al obtener los datos:", e);
     }
   };
   const handleDeleteClick = async () => {
     try {
-      const response = await clientAxios.delete(`tasks/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      Swal.fire({
-        title: "Alert!",
-        text: "se ha eliminado correctamente",
-        icon: "success",
-        confirmButtonText: "success",
-      });
-      console.log(response);
-      setTimeout(() => {
-        window.location.replace("/");
-      }, 1000);
 
+      await deleteTask(id)
       console.log("elimidano correctamente");
     } catch (e) {
       console.error("Error al obtener los datos:", e);
@@ -87,42 +56,18 @@ export const Card = ({ title, body, color, id, state, date }) => {
         <p className="date">Date: {fechaFormateada}</p>
         <div className="right ">
           <AnchorIcon
-
             onClick={handleClick}
             style={{ color: state === "anchored" ? "red" : "white" }}
           />
         </div>
       </div>
-      <div className="input-container">
-        <div className="input-container">
-          <input
-            className="title border"
-            id="title"
-            type="text"
-            placeholder=" "
-            value={cardTitle}
-            onChange={(e) => setCardTitle(e.target.value)}
-          />
-          <span className="floating-label">Title</span>
-        </div>
-        <div className="input-container">
-          <textarea
-            className="description border"
-            placeholder=" "
-            value={cardDescriptions}
-            onChange={(e) => setDescriptions(e.target.value)}
-          />
-          <span className="floating-label">Description</span>
-        </div>
-
-        <div className="input-container">
-          <Link to="/">
-            <button className="add-card-btn" onClick={handleClick}>
-              edit
-            </button>
-          </Link>
-        </div>
-      </div>
+      <CardForm
+        cardTitle={cardTitle}
+        setCardTitle={setCardTitle}
+        cardDescriptions={cardDescriptions}
+        setDescriptions={setDescriptions}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
