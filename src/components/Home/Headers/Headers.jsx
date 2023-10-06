d4708a7a58a895e25d24a1cba4c5140e7743dae1
Miguel Velasco <migue16velasco@gmail.com>
2023-10-06 10:23:45 -0400

    feat:<headers/headers.jsx> remove menu

--- src/components/Home/Headers/Headers.jsx
+++ src/components/Home/Headers/Headers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Menu } from "./Menu";
-// import { Search } from "../New Task/Search";
+
 import { Logout } from "../New Task/Logout";
 import "./style.css";
 
@@ -9,13 +8,12 @@ export function Headers() {
     <div className="header-container">
       <div className="barra-list">
         <div className="menu-section">
-          <Menu />
+
         </div>
         <div className="title-section">
           <h1 className="title-headers">Task Manager</h1>
         </div>
         <div className="search-section">
-          {/* <Search /> */}
           <Logout />
         </div>
       </div>
